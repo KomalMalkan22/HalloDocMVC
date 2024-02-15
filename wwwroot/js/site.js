c88992d07bb8a1ee18c83d1cb38909d62d6c95b1
Komal Malkan <tatva.dotnet.komalmalkan@outlook.com>
2024-02-15 13:55:03 +0530

    Mobile view created of Dashboard and View Documents Page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-/* Confirmation Modal */
+﻿/* Confirmation Modal */
 var targetModal = new bootstrap.Modal(document.getElementById('targetModal', {}))
 targetModal.show();
 
